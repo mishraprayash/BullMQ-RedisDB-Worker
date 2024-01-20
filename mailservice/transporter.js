a4eb06c587b7fcf0072ff5b4c16d599373ed3a7e
Prayash Mishra <mishraprayash11@gmail.com>
2024-01-20 09:44:47 +0545

    created transporter for mailservice

---
+++ mailservice/transporter.js
@@ -0,0 +1,15 @@
+
+const nodemailer = require("nodemailer");
+
+const createTransporter = () => {
+    return nodemailer.createTransport({
+        host: process.env.SMTP_HOST,
+        port: 2525,
+        auth: {
+            user: process.env.AUTH_USER,
+            pass: process.env.AUTH_PASSWORD,
+        },
+    });
+}
+
+module.exports = createTransporter
