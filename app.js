d0bee3fe16552cb431ac541835ae9201b2b8ebb8
Prayash Mishra <mishraprayash11@gmail.com>
2024-01-29 21:57:12 +0545

    imported file inside the http server

--- app.js
+++ app.js
@@ -2,11 +2,9 @@
 const http = require('node:http')
 require('dotenv').config()
 
-const emailWorker = require('./queues/emailQueue');
-
 //create a server object:
 http.createServer(function (req, res) {
-    console.log(req);
+    const emailWorker = require('./queues/emailQueue');
     res.write('Hello World!'); //write a response to the client
     res.end(); //end the response
 }).listen(8080); //the server object listens on port 8080
