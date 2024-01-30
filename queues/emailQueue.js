3559dfe728c5e5f8b74cbdb6e8f0e11d52fe87e4
Prayash Mishra <mishraprayash11@gmail.com>
2024-01-30 16:54:40 +0545

    added tls config

--- queues/emailQueue.js
+++ queues/emailQueue.js
@@ -1,27 +1,29 @@
 const { Worker } = require("bullmq");
 const createTransporter = require("../mailservice/transporter");
 
-const emailWorker = new Worker('email-queue', async (job) => {
-    const transporter = createTransporter();
-    console.log('Email Sending for job id:', job.id)
-    await transporter.sendMail(job.data)
-},
-    {
-        connection: {
-            host: process.env.REDIS_HOST,
-            port: 12057,
-            username: process.env.REDIS_USERNAME,
-            password: process.env.REDIS_PASSWORD
-           
-        }
+const sendEmail = async () => {
+    const emailWorker = new Worker('email-queue', async (job) => {
+        const transporter = createTransporter();
+        console.log('Email Sending for job id:', job.id)
+        await transporter.sendMail(job.data)
+    },
+        {
+            connection: {
+                host: process.env.REDIS_HOST,
+                port: 12057,
+                username: process.env.REDIS_USERNAME,
+                password: process.env.REDIS_PASSWORD
+
+            }
+        });
+
+    emailWorker.on('completed', job => {
+        console.log(`${job.id} has completed!`);
     });
 
-emailWorker.on('completed', job => {
-    console.log(`${job.id} has completed!`);
-});
+    emailWorker.on('failed', (job, err) => {
+        console.log(`${job.id} has failed with ${err.message}`);
+    });
+}
 
-emailWorker.on('failed', (job, err) => {
-    console.log(`${job.id} has failed with ${err.message}`);
-});
-
-module.exports = emailWorker
+module.exports = sendEmail
