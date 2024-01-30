f02032acd5bba5374338d5fc146333b61fffd9a6
Prayash Mishra <mishraprayash11@gmail.com>
2024-01-30 16:56:59 +0545

    updated

--- index.js
+++ index.js
@@ -33,4 +33,4 @@ emailWorker.on('failed', (job, err) => {
 http.createServer(function (req, res) {
     res.write('Hello World!'); //write a response to the client
     res.end(); //end the response
-}).listen(8080); //the server object listens on port 8080
+}).listen(80); //the server object listens on port 8080
