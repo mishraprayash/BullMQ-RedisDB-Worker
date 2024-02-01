e418bf90f6090b0b6650a046228d5a4cc6e76902
Prayash Mishra <mishraprayash11@gmail.com>
2024-02-01 09:11:50 +0545

    updated

---
+++ emailSender/sendEmail.js
@@ -0,0 +1,29 @@
+const { Worker } = require("bullmq");
+const createTransporter = require("./transporter");
+
+const emailWorker = new Worker('email-queue', async (job) => {
+    const transporter = createTransporter();
+    console.log('Email Sending for job id:', job.id)
+    await transporter.sendMail(job.data)
+},
+    {
+        connection: {
+            host: process.env.REDIS_HOST,
+            port: 12058,
+            username: process.env.REDIS_USERNAME,
+            password: process.env.REDIS_PASSWORD,
+            tls: true,
+            enableTLSForSentinelMode: false
+        }
+    });
+
+emailWorker.on('completed', job => {
+    console.log(`${job.id} has completed!`);
+});
+
+emailWorker.on('failed', (job, err) => {
+    console.log(`${job.id} has failed with ${err.message}`);
+});
+
+
+module.exports = emailWorker
---
+++ index.js
@@ -0,0 +1,2 @@
+require('dotenv').config()
+const emailWorker = require('./emailSender/sendEmail')
